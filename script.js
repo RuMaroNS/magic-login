b8351c5669cd308ac7313ec3b90250e50cbbace5
RuMaroNS <sdulimos@gmail.com>
2026-04-15 17:34:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Инициализация EmailJS (вставь свой Public Key)
+emailjs.init("gTrqvbOiCTqlJcDNJ");
+
+let generatedOTP;
+
+function sendOTP() {
+    const email = document.getElementById('user_email').value;
+    if (!email) return alert("Введите почту!");
+
+    // Генерируем 4-значный код
+    generatedOTP = Math.floor(1000 + Math.random() * 9000);
+
+    const templateParams = {
+        to_email: email,
+        otp_code: generatedOTP
+    };
+
+    // Отправка через EmailJS
+    emailjs.send('service_j9ls8lo', 'template_ebxnpr6', templateParams)
+        .then(() => {
+            alert("Код отправлен!");
+            document.getElementById('step-1').style.display = 'none';
+            document.getElementById('step-2').style.display = 'block';
+        }, (error) => {
+            console.log('Ошибка:', error);
+            alert("Не удалось отправить код.");
+        });
+}
+
+function verifyOTP() {
+    const userInput = document.getElementById('otp_input').value;
+    
+    if (userInput == generatedOTP) {
+        alert("Успешный вход! Добро пожаловать.");
+        // Здесь можно перенаправить на другую страницу
+        window.location.href = "https://your-cool-game.com";
+    } else {
+        alert("Неверный код. Попробуй еще раз.");
+    }
+}
